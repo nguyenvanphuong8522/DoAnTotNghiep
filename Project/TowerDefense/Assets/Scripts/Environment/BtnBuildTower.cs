966628e4dfc11fc31ed686e893183ef274fe00ab
NguyenVanPhuong <nguyenvanphuong8522@gmail.com>
2024-05-04 21:56:37 +0700

    chore: add icon demo

--- Project/TowerDefense/Assets/Scripts/Environment/BtnBuildTower.cs
+++ Project/TowerDefense/Assets/Scripts/Environment/BtnBuildTower.cs
@@ -8,14 +8,25 @@ public class BtnBuildTower : MonoBehaviour
     public int indexTower;
     public ButtonEffectLogic btn;
     public UiBuildTower uiBuilder;
+    [SerializeField] private int price;
     private void Start()
     {
         btn.onClick.AddListener(BuildTower);
     }
     public void BuildTower()
     {
-        ObjectPool.instance.Get(ObjectPool.instance.towers[indexTower].pools[0], uiBuilder.main.position);
-        uiBuilder.Hide();
+        if(LevelManager.instance.curLevel.money >= price)
+        {
+            LevelManager.instance.curLevel.money -= price;
+            UiGameplay.instance.UpdatTxtMoney();
+            ObjectPool.instance.Get(ObjectPool.instance.towers[indexTower].pools[0], uiBuilder.main.position);
+            uiBuilder.Hide();
+        }
+        else
+        {
+            Debug.Log("not enough money");
+        }
+        
     }
     
 }
